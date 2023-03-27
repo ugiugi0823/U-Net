04603a273977b4b9e501602615826da979670c58
Hyunwook Jo <acerghjk@gmail.com>
2023-03-27 11:35:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
 
+
+from src.model import UNet
+
+
 torch.manual_seed(0)
 criterion = nn.BCEWithLogitsLoss() # loss
 n_epochs = 200 
@@ -21,6 +25,31 @@ target_shape = 373
 device = 'cuda'
 
 
+
+
+# UNQ_C2 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: crop
+def crop(image, new_shape):
+    '''
+    Function for cropping an image tensor: Given an image tensor and the new shape,
+    crops to the center pixels (assumes that the input's size and the new size are
+    even numbers).
+    Parameters:
+        image: image tensor of shape (batch size, channels, height, width)
+        new_shape: a torch.Size object with the shape you want x to have
+    '''
+    # There are many ways to implement this crop function, but it's what allows
+    # the skip connection to function as intended with two differently sized images!
+    #### START CODE HERE ####
+    middle_height = image.shape[2] // 2
+    middle_width = image.shape[3] // 2
+    starting_height = middle_height - new_shape[2] // 2
+    final_height = starting_height + new_shape[2]
+    starting_width = middle_width - new_shape[3] // 2
+    final_width = starting_width + new_shape[3]
+    cropped_image = image[:, :, starting_height:final_height, starting_width:final_width]
+    #### END CODE HERE ####
+    return cropped_image
 
 
 
